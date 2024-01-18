099e6cafddcf29d0bd9b75c4e7e1b50e2805341c
Harsh Agrawal <harshiscoding@gmail.com>
2024-01-18 10:51:12 +0530

    youtube button added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,39 +1,48 @@
-import React from 'react';
+import React from "react";
 import { FaFacebook } from "react-icons/fa";
 import { IoLogoInstagram } from "react-icons/io5";
+import { FaYoutube } from "react-icons/fa";
 const Header = () => {
-    const headerStyle = {
-        maxHeight: 'fit-content', // Adjust the maximum width as needed
-      };
-    const logoStyle = {
-        width: '100px', // Adjust the maximum width as needed
-        height:'100px'
-      };
-      const brandNameStyle={
-        fontSize:'30px',
-        marginRight:'20px',
-      };
+  const headerStyle = {
+    maxHeight: "fit-content", // Adjust the maximum width as needed
+  };
+  const logoStyle = {
+    width: "100px", // Adjust the maximum width as needed
+    height: "100px",
+  };
+  const brandNameStyle = {
+    fontSize: "30px",
+    marginRight: "20px",
+  };
 
-      const socialMediaIconsStyle = {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'flex-end'
-      };
-    
-      const iconStyle = {
-        fontSize: '40px', // Adjust the icon size as needed
-        marginRight: '10px', // Adjust the margin between icons as needed
-      };
-    
-    
+  const socialMediaIconsStyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+  };
+
+  const iconStyle = {
+    fontSize: "40px", // Adjust the icon size as needed
+    marginRight: "10px", // Adjust the margin between icons as needed
+  };
+
   return (
     <header className="p-3 bg-dark text-white" style={headerStyle}>
       <div className="container">
         <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/"  className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-            <img src="src/assets/logo-dham-yatara.png" alt="Logo" width="40" height="32" style={logoStyle} />
+          <a
+            href="/"
+            className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
+          >
+            <img
+              src="src/assets/logo-dham-yatara.png"
+              alt="Logo"
+              width="40"
+              height="32"
+              style={logoStyle}
+            />
           </a>
-          <div style={brandNameStyle} >Shri Dham Yatra</div>
+          <div style={brandNameStyle}>Shri Dham Yatra</div>
 
           {/* <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
             <li><a href="#" className="nav-link px-2 text-secondary">Home</a></li>
@@ -43,17 +52,39 @@ const Header = () => {
             <li><a href="#" className="nav-link px-2 text-white">About</a></li>
           </ul> */}
 
-         <div className='col-md-7 text-end'>
-            <a  target="_blank" rel="noopener noreferrer" className='text-decoration-none text-white' href="https://www.facebook.com/ShriDhamyatara"> <FaFacebook style={iconStyle}/></a>
-        <a  target="_blank" rel="noopener noreferrer" className='text-decoration-none text-white' href="https://www.instagram.com/shridhamyatra/?igsh=YWYwM2I1ZDdmOQ%3D%3D"> <IoLogoInstagram style={iconStyle}/></a>
-        
-
-         </div>
-        
+          <div className="col-md-7 text-end">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-decoration-none text-white"
+              href="https://www.facebook.com/ShriDhamyatara"
+            >
+              {" "}
+              <FaFacebook style={iconStyle} />
+            </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-decoration-none text-white"
+              href="https://www.instagram.com/shridhamyatra/?igsh=YWYwM2I1ZDdmOQ%3D%3D"
+            >
+              {" "}
+              <IoLogoInstagram style={iconStyle} />
+            </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-decoration-none text-white"
+              href="https://www.youtube.com/@Shridham-yatra"
+            >
+              {" "}
+              <FaYoutube style={iconStyle} />
+            </a>
+          </div>
         </div>
       </div>
     </header>
   );
-}
+};
 
 export default Header;
